87774daa4dac6fe59094dcc8f5b310ce943c94a9
gustavobiondi <168185680+gustavobiondi@users.noreply.github.com>
2025-04-13 08:21:35 -0300

    Delete flask-server directory

--- flask-server/manipule.py
+++
@@ -1,9 +0,0 @@
-from cs50 import SQL
-db = SQL('sqlite:///dados.db')
-import re
-from datetime import datetime
-hoje = datetime.now().date()
-
-db.execute("UPDATE pagamentos SET faturamento = 0 WHERE dia = ?", hoje)
-s = db.execute("SELECT * FROM pagamentos")
-print(s)
