d7852a05c1b782017e1b2e53e4738a89394940c4
gustavobiondi <168185680+gustavobiondi@users.noreply.github.com>
2025-04-16 19:54:26 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ logging.getLogger('matplotlib').setLevel(logging.WARNING)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'seu_segredo_aqui'
 socketio = SocketIO(app, cors_allowed_origins="*")
-db = SQL("sqlite:////data/dados.db")
+db = SQL("sqlite:///data/dados.db")
 CORS(app, resources={r"/*": {"origins": "*"}})  # Permite todas as origens
 brazil = timezone('America/Sao_Paulo')
 
