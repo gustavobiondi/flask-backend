2c679e5359df5a3561e9a33bdb7f6a2121fe9f70
gustavobiondi <168185680+gustavobiondi@users.noreply.github.com>
2025-04-16 21:36:48 -0300

    Add files via upload

--- manipule.py
+++ manipule.py
@@ -13,5 +13,5 @@ dia = datetime.now().date()
 
 
 
-db.execute("UPDATE cardapio SET opcoes = ? WHERE item = ?",'Frutas(abacaxi-acai-banana com canela-caju-kiwi-limao-limao siciliano-lima da persia-manga-maracuja-melancia-morango-tangerina)Adicional(leite condensado)','item')
+db.execute("UPDATE cardapio SET opcoes = ? WHERE item = ?",'Frutas(abacaxi-acai-banana com canela-caju-kiwi-limao-limao siciliano-lima da persia-manga-maracuja-melancia-morango-tangerina)Adicional(leite condensado)','suco')
 print(db.execute("SELECT * FROM cardapio WHERE item = 'suco'"))
