977a607b10acb5b6c695e9d54c250bc9ad294ac0
gustavobiondi <168185680+gustavobiondi@users.noreply.github.com>
2025-04-17 23:26:05 -0300

    Add files via upload

--- manipule.py
+++ manipule.py
@@ -10,10 +10,9 @@ if not os.path.exists(DATABASE_PATH):
 db = SQL("sqlite:///" + DATABASE_PATH)
 dia = datetime.now().date()
 
-
-db.execute('UPDATE estoque_geral SET item = ? WHERE estoque_ideal = ? AND quantidadae = ?','agua',588,226)
-db.execute('UPDATE estoque_geral SET item = ? WHERE estoque_ideal = ? AND quantidadae = ?','agua c gas',288,144)
-db.execute('UPDATE estoque_geral SET item = ? WHERE estoque_ideal = ? AND quantidadae = ?','agua tonica',60,48)
-db.execute('UPDATE estoque_geral SET item = ? WHERE estoque_ideal = ? AND quantidadae = ?','tang',64,160)
+db.execute('UPDATE estoque_geral SET item = ? WHERE estoque_ideal = ? AND quantidade = ?','agua',588,226)
+db.execute('UPDATE estoque_geral SET item = ? WHERE estoque_ideal = ? AND quantidade = ?','agua c gas',288,144)
+db.execute('UPDATE estoque_geral SET item = ? WHERE estoque_ideal = ? AND quantidade = ?','agua tonica',60,48)
+db.execute('UPDATE estoque_geral SET item = ? WHERE estoque_ideal = ? AND quantidade = ?','tang',64,160)
 
 print(db.execute('SELECT * FROM estoque_geral'))
