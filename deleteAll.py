f2486a0f6ad4cb0d0f150a4b15306c99ca4fd75f
gustavobiondi <168185680+gustavobiondi@users.noreply.github.com>
2025-04-27 20:20:32 -0300

    Add files via upload

--- deleteAll.py
+++ deleteAll.py
@@ -1,7 +1,24 @@
 from cs50 import SQL
+import os
+import shutil
 
-db=SQL('sqlite:///data/dados.db')
+DATABASE_PATH = "/data/dados.db"
 
-db.execute('CREATE TABLE alteracoes (tabela TEXT, alteracao TEXT, tipo TEXT, usuario TEXT, tela TEXT, dia TEXT, horario TEXT)')
+if not os.path.exists(DATABASE_PATH):
+    shutil.copy("dados.db", DATABASE_PATH)
+
+db = SQL("sqlite:///" + DATABASE_PATH)
+
+db.execute('''
+    CREATE TABLE IF NOT EXISTS alteracoes (
+        tabela TEXT,
+        alteracao TEXT,
+        tipo TEXT,
+        usuario TEXT,
+        tela TEXT,
+        dia TEXT,
+        horario TEXT
+    )
+''')
 
 print(db.execute('SELECT * FROM alteracoes'))
