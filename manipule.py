25a521026cf14bcb1b1cbf1c65ed31b7dbe9744b
gustavobiondi <168185680+gustavobiondi@users.noreply.github.com>
2025-05-07 19:53:36 -0300

    Add files via upload

--- manipule.py
+++ manipule.py
@@ -4,7 +4,10 @@ import shutil
 import os
 
 
-db = SQL("sqlite:///data/dados.db")
+DATABASE_PATH = "/data/dados.db"
+if not os.path.exists(DATABASE_PATH):
+    shutil.copy("dados.db", DATABASE_PATH)
+db = SQL("sqlite:///" + DATABASE_PATH)
 
 hoje = datetime.now().date()
 
