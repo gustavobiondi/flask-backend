2e991da8dabd4cea5c2a83c8f23637f617a0dc1b
gustavobiondi <168185680+gustavobiondi@users.noreply.github.com>
2025-05-07 20:01:46 -0300

    Add files via upload

--- manipule.py
+++ manipule.py
@@ -11,6 +11,4 @@ db = SQL("sqlite:///" + DATABASE_PATH)
 
 hoje = datetime.now().date()
 
-db.execute('DROP TABLE tokens')
-db.execute("INSERT INTO pagamentos (dia,faturamento, faturamento_prev, drinks, porcoes, restantes, totais_pedidos,caixinha) VALUES (?,0, 0, 0, 0, 0, 0,0)", hoje)
 db.execute("CREATE TABLE IF NOT EXISTS tokens (token TEXT NOT NULL,cargo TEXT, username TEXT)")
