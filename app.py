c548b9cb5ea517a8b64d08fe7800265719fbdde7
gustavobiondi <168185680+gustavobiondi@users.noreply.github.com>
2025-05-27 07:24:35 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -953,7 +953,7 @@ def handle_get_cardapio(data):
             fcomanda = data.get('fcomanda')
             ordem = data.get('ordem')
         if ordem == 0:
-            valor_pago = db.execute('SELECT SUM(valor) AS total FROM pagamentos WHERE comanda = ? AND ordem = ? AND dia = ? AND tipo = ?', fcomanda, ordem,dia,'normal')
+            valor_pago = db.execute('SELECT SUM(valor) AS total FROM pagamentos WHERE comanda = ? AND ordem = ? AND dia = ? AND tipo IN (?,?)', fcomanda, ordem,dia,'normal','desconto')
             preco_pago = 0
             if valor_pago and valor_pago[0]['total']:
                 preco_pago = float(valor_pago[0]['total']),
